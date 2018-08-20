af676dd6c2d0b40d4f8e416bc3d884655db2e8a2
Chu Vu <chuvanvu1324@gmail.com>
2018-08-20 20:51:33 +0700

    update route

--- routes/site.js
+++ routes/site.js
@@ -1,13 +1,27 @@
 var router = (app) => {
+	var PostController = require('../controllers/admin/PostController');
 
 
 	app.get('/', (req, res) => {
-		res.render('site/home', {data: {title: "Trang chủ"}});
+		PostController.getAll().then(rows => {
+			console.log(rows);
+			res.render('site/home', {data: {title: "Trang chủ", rows}});
+		});
 	})
 	app.get('/post-detail', (req, res) => {
 		res.render('site/post-detail', {data: {title: "Chi tiết bài viết"}});
-	})
+	});
 
+	//create breadcrum post detao;
+
+	PostController.getAll().then(rows => {
+		rows.forEach(row => {
+			app.get('/' + row.post_slug, (req, res) => {
+				res.end(""+row.post_slug);
+
+			})
+		});
+	});
 	//logout
 	app.get('/logout', (req, res) => {
 	    req.logout();
