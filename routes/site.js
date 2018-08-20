d53dcc8615f72e8fc2e48bd1993ba8e31b6248be
Chu Vu <chuvanvu1324@gmail.com>
2018-08-20 22:18:14 +0700

    update route

--- routes/site.js
+++ routes/site.js
@@ -4,24 +4,32 @@ var router = (app) => {
 
 	app.get('/', (req, res) => {
 		PostController.getAll().then(rows => {
-			console.log(rows);
+			// console.log(rows);
 			res.render('site/home', {data: {title: "Trang chủ", rows}});
+			rows.forEach(row => {
+				app.get('/' + row.post_slug, (req, res) => {
+					res.render('site/post-detail', {data: {title : row.post_title, row}});
+					// res.end(""+row.post_slug, {data: row});
+
+				})
+			});
 		});
 	})
 	app.get('/post-detail', (req, res) => {
 		res.render('site/post-detail', {data: {title: "Chi tiết bài viết"}});
 	});
 
-	//create breadcrum post detao;
+	//create breadcrum post detail;
 
-	PostController.getAll().then(rows => {
-		rows.forEach(row => {
-			app.get('/' + row.post_slug, (req, res) => {
-				res.end(""+row.post_slug);
+	// PostController.getAll().then(rows => {
+	// 	rows.forEach(row => {
+	// 		app.get('/' + row.post_slug, (req, res) => {
+	// 			res.render('site/post-detail', {data: {title : row.post_title, row}});
+	// 			// res.end(""+row.post_slug, {data: row});
 
-			})
-		});
-	});
+	// 		})
+	// 	});
+	// });
 	//logout
 	app.get('/logout', (req, res) => {
 	    req.logout();
